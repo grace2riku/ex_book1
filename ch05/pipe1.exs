3873b7c63fca6d41130a7cf9de7eaa974a661000
Koji Abe <gracehime@gmail.com>
2023-02-25 22:15:38 +0900

    p116。パイプ演算子を使う。パイプ演算子は左辺を評価して得た値を右辺の関数の第1引数に渡す。

--- ch05/pipe1.exs
+++ ch05/pipe1.exs
@@ -5,8 +5,10 @@ defmodule MyMod do
   end
 end
 
-n = 2
-n = MyMod.double(n)
-n = MyMod.double(n)
-n = MyMod.double(n)
+n =
+  2
+  |> MyMod.double()
+  |> MyMod.double()
+  |> MyMod.double()
+
 IO.puts(n)

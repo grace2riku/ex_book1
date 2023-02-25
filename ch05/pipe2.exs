707122ccae9d640cfbd09eff2c728405eac4330c
Koji Abe <gracehime@gmail.com>
2023-02-26 00:28:10 +0900

    p117。パイプで複数の関数呼び出しをしなくても良いことを体験できた。

--- ch05/pipe2.exs
+++ ch05/pipe2.exs
@@ -5,9 +5,15 @@ defmodule MyMod do
   end
 end
 
-n = 2
-n = MyMod.multiply(n, 3)
-n = MyMod.multiply(n, 5)
-n = MyMod.multiply(n, 7)
+#n = 2
+#n = MyMod.multiply(n, 3)
+#n = MyMod.multiply(n, 5)
+#n = MyMod.multiply(n, 7)
+
+n =
+  2
+  |> MyMod.multiply(3)
+  |> MyMod.multiply(5)
+  |> MyMod.multiply(7)
 
 IO.puts(n)

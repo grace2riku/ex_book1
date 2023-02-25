72affaca7586aad1450039b2dac2121784ccac2d
Koji Abe <gracehime@gmail.com>
2023-02-25 23:00:30 +0900

    p117。関数を3回呼び出す。引数が2つの関数を複数回呼び出す。

---
+++ ch05/pipe2.exs
@@ -0,0 +1,13 @@
+#!/usr/bin/env elixir
+defmodule MyMod do
+  def multiply(x, y) do
+    x * y
+  end
+end
+
+n = 2
+n = MyMod.multiply(n, 3)
+n = MyMod.multiply(n, 5)
+n = MyMod.multiply(n, 7)
+
+IO.puts(n)

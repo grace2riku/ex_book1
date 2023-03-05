04919a6cff996ca154b19092e379bf8efb0fb660
Koji Abe <gracehime@gmail.com>
2023-03-06 08:58:24 +0900

    p204のリストを動作確認した。apply/2関数で前回のリストをすっきり実装できた例。

--- ch08/apply1.exs
+++ ch08/apply1.exs
@@ -2,6 +2,11 @@
 list = [1, 2]
 add = fn a, b -> a + b end
 
-[i, j] = list
-x = add.(i, j)
+#[i, j] = list
+#x = add.(i, j)
+
+# p204 apply/2関数を使えば無名関数とリストを引数にセットし呼び出せる。
+# ↑の2行のようにリストの値を個別の変数にセットしなくよくなる。
+x = apply(add, list)
+
 IO.inspect(x)

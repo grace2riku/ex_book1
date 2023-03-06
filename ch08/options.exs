3399968fe6e5b5191845f2cd55066d8fb647f80b
Koji Abe <gracehime@gmail.com>
2023-03-07 08:46:56 +0900

    p215のリストを動作確認した。オプション引数の例題。

---
+++ ch08/options.exs
@@ -0,0 +1,15 @@
+#!/usr/bin/env elixir
+defmodule MyMod do
+  def hello(name, opts \\[]) do
+    call = opts[:word] || "Hello"
+    mark = opts[:mark] || "!"
+    IO.puts(call <> ", " <> name <> mark)
+  end
+end
+
+MyMod.hello("Alice")
+MyMod.hello("Alice", [word: "Bonjour"])
+MyMod.hello("Alice", [word: "Ciao", mark: "!!"])
+
+IO.inspect(MyMod.__info__(:functions))
+IO.inspect(MyMod.__info__(:functions), label: "【label:ありバージョン】MyMod module funcion list")

844c9def9cd697385553000f72f3c9ac6fc20ee1
Koji Abe <gracehime@gmail.com>
2023-03-06 09:09:58 +0900

    p205のリストを動作確認した。hello/0, hello/1関数がある。アリティ(引数の個数)が異なるので別の関数として扱われる。

---
+++ ch08/named_func1.exs
@@ -0,0 +1,14 @@
+#!/usr/bin/env elixir
+defmodule MyMod do
+  def hello do
+    "Hello!"
+  end
+
+  def hello(name) do
+    "Hello, #{name}!"
+  end
+
+end
+
+IO.puts(MyMod.hello())
+IO.puts(MyMod.hello("Alice"))

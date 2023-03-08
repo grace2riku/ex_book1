33dc1837957ad66c2a0af4d436142ce76d041573
Koji Abe <gracehime@gmail.com>
2023-03-08 21:09:26 +0900

    p228のリストを確認した。プライベート関数は「同じモジュールに属する関数からであってもリモート呼び出し(※)はできない」という制限を確認(エラーになること)した。
    ※モジュール名付きの関数呼び出し(例ではfunc1でプライベート関数func2をMyMod.func2()で呼び出す)をリモート呼び出し(remote_call)という。

--- ch09/two_funcs.exs
+++ ch09/two_funcs.exs
@@ -1,12 +1,9 @@
 #!/usr/bin/env elixir
 
 defmodule MyMod do
-  def func1, do: func2()
-#  def func2, do: :ok
+#  def func1, do: func2()
+  def func1, do: MyMod.func2()
   defp func2, do: :ok
 end
-
-# func2はプライベート関数のためモジュールの外側からは呼べない。つぎのコードを有効にするとエラーになる。
-#IO.inspect(MyMod.func2())
 
 IO.inspect(MyMod.func1())

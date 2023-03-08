7684a6df7bdd76eccdd142aaddceb666547516e1
Koji Abe <gracehime@gmail.com>
2023-03-08 21:34:49 +0900

    p232のリストを確認した。プライベート関数に@docで説明文章があると警告がでることを確認した。警告はでるがプログラムの実行はできる。

--- ch09/two_funcs.exs
+++ ch09/two_funcs.exs
@@ -4,6 +4,9 @@ defmodule MyMod do
   def func1, do: func2()
 #  def func1, do: MyMod.func2() # プライベート関数はリモート呼び出しできないためエラーとなる
 #  def func1, do: apply(MyMod, :func2, []) # apply/3関数でもプライベート関数の呼び出しはエラーとなる
+  @doc """
+  `:ok`を返す
+  """
   defp func2, do: :ok
   defp func3, do: :bang
 end

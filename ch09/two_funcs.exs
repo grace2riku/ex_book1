a4e6eded31ae7c4addf1fb99135a9031ce7987e1
Koji Abe <gracehime@gmail.com>
2023-03-08 21:27:28 +0900

    p231のリストを確認した。利用されないプライベート関数があると警告がでることを確認した。警告はでるがプログラムの実行はできる。

--- ch09/two_funcs.exs
+++ ch09/two_funcs.exs
@@ -1,10 +1,11 @@
 #!/usr/bin/env elixir
 
 defmodule MyMod do
-#  def func1, do: func2()
+  def func1, do: func2()
 #  def func1, do: MyMod.func2() # プライベート関数はリモート呼び出しできないためエラーとなる
-  def func1, do: apply(MyMod, :func2, []) # apply/3関数でもプライベート関数の呼び出しはエラーとなる
+#  def func1, do: apply(MyMod, :func2, []) # apply/3関数でもプライベート関数の呼び出しはエラーとなる
   defp func2, do: :ok
+  defp func3, do: :bang
 end
 
 IO.inspect(MyMod.func1())

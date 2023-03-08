f611706fbf745e61a860f16ca4e5648d4360daf0
Koji Abe <gracehime@gmail.com>
2023-03-08 21:23:40 +0900

    p230のリストを確認した。プライベート関数はapply/3関数経由で呼び出しでもエラーになることを確認した。

--- ch09/two_funcs.exs
+++ ch09/two_funcs.exs
@@ -2,7 +2,8 @@
 
 defmodule MyMod do
 #  def func1, do: func2()
-  def func1, do: MyMod.func2()
+#  def func1, do: MyMod.func2() # プライベート関数はリモート呼び出しできないためエラーとなる
+  def func1, do: apply(MyMod, :func2, []) # apply/3関数でもプライベート関数の呼び出しはエラーとなる
   defp func2, do: :ok
 end
 

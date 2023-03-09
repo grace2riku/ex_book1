686fd0e8180f83bbd4497e6afa3e5c52f67dfc52
Koji Abe <gracehime@gmail.com>
2023-03-09 12:32:12 +0900

    p233のリストを確認した。パブリック関数の有無を調べる関数(function_exported?/3)でパブリック関数の有無を調べた。パブリック関数func1はtrue, プライベート変数func2はfalseが返ることを確認した。 __info__(:functions)ではパブリック関数の名前とアリティの組を含むキーワードリストが返された。プライベート関数func2の組は返されないことを確認した。

--- ch09/two_funcs.exs
+++ ch09/two_funcs.exs
@@ -2,13 +2,10 @@
 
 defmodule MyMod do
   def func1, do: func2()
-#  def func1, do: MyMod.func2() # プライベート関数はリモート呼び出しできないためエラーとなる
-#  def func1, do: apply(MyMod, :func2, []) # apply/3関数でもプライベート関数の呼び出しはエラーとなる
-  @doc """
-  `:ok`を返す
-  """
   defp func2, do: :ok
-  defp func3, do: :bang
 end
 
 IO.inspect(MyMod.func1())
+IO.inspect(function_exported?(MyMod, :func1, 0))
+IO.inspect(function_exported?(MyMod, :func2, 0))
+IO.inspect(MyMod.__info__(:functions))

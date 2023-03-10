c84aa9011fe6625e0a901ae3d8218631bb612719
Koji Abe <gracehime@gmail.com>
2023-03-10 16:13:40 +0900

    p236のリストを確認した。同じモジュール内にある他の関数をキャプチャした。この場合はモジュール名を指定せずにキャプチャした関数を呼び出せる。

---
+++ ch09/capture2.exs
@@ -0,0 +1,13 @@
+#!/usr/bin/env elixir
+defmodule MyMod do
+  def experiment do
+    func = &multiply/2
+    func.(3, 5)
+  end
+
+  def multiply(a, b) do
+    a * b
+  end
+end
+
+IO.inspect(MyMod.experiment())

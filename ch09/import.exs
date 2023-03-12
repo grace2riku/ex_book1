dbe35798746d5235ed6e4f32a2fdec17dfad8442
Koji Abe <gracehime@gmail.com>
2023-03-12 10:58:48 +0900

    p244のリストを確認した。importしたモジュールの関数は取り込み先のモジュールのパブリック関数にならないことを確認した。つぎのエラーになる。
    $ ./import.exs
    ** (UndefinedFunctionError) function MyMod2.func1/0 is undefined or private
        MyMod2.func1()
        import.exs:20: (file)

--- ch09/import.exs
+++ ch09/import.exs
@@ -5,11 +5,16 @@ defmodule MyMod1 do
 end
 
 defmodule MyMod2 do
-  import MyMod1
+#️  import MyMod1
+  import MyMod1, only: [func1: 0, func2: 0]
 
   def experiment do
     func1() + func2()
   end
 end
 
-IO.inspect(MyMod2.experiment)
+#IO.inspect(MyMod2.experiment)
+
+#MyMod1を取り込んでも関数func1/0がMyMod2のパブリック変数になるわけではない。
+#つぎはエラーになる
+IO.inspect(MyMod2.func1())

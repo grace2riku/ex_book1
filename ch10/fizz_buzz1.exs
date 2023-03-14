5ea1e82220035f94646eef32c725a8a5fc6040dd
Koji Abe <gracehime@gmail.com>
2023-03-14 13:06:33 +0900

    p274のリストを確認した。FizzBuzz問題のElixir実装。if文の条件分岐が多くて読みにくかった。Elixirのif文はelsif節はない。condマクロを用いるともっと簡潔に書けるとのこと。

---
+++ ch10/fizz_buzz1.exs
@@ -0,0 +1,16 @@
+#!/usr/bin/env elixir
+for i <- 1..20 do
+  if Integer.mod(i, 15) == 0 do
+    IO.puts("Fizz Buzz")
+  else
+    if Integer.mod(i, 3) == 0 do
+      IO.puts("Fizz")
+    else
+      if Integer.mod(i, 5) == 0 do
+        IO.puts("Buzz")
+      else
+        IO.puts(i)
+      end
+    end
+  end
+end

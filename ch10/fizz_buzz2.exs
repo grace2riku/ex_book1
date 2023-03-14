e90ae6d5cee871c35e80f016a9fdd9b7d219c919
Koji Abe <gracehime@gmail.com>
2023-03-14 21:48:55 +0900

    p282のリストを確認した。condマクロを使うことでifマクロよりすっきり簡潔なコードになることを確認できた。

---
+++ ch10/fizz_buzz2.exs
@@ -0,0 +1,9 @@
+#!/usr/bin/env elixir
+for i <- 1..20 do
+  cond do
+    Integer.mod(i, 15) == 0 -> IO.puts("Fizz Buzz")
+    Integer.mod(i, 3) == 0 -> IO.puts("Fizz")
+    Integer.mod(i, 5) == 0 -> IO.puts("Buzz")
+    true -> IO.puts(i)
+  end
+end
